70c2b331e318fcd25eec8656a75319f469a39e76
Ayman Jibrael <aymanjibrael@outlook.com>
2023-01-19 14:40:06 +0300

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Ayman on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
